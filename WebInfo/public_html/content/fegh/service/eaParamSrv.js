0bb9921b53382299c715cdccbd237f7ed897a786
Egbert <Egbert@LenoAleks>
2023-01-18 17:19:30 +0100

    Split app and content 2

--- WebInfo/public_html/content/fegh/service/eaParamSrv.js
+++ WebInfo/public_html/content/fegh/service/eaParamSrv.js
@@ -34,7 +34,7 @@ var getNewsList = function() {
             newTo: "2023-03-30",
             title: "Wir erhalten Landesförderung!", 
             body: ["Mit unserer geplanten Photovoltaik-Freiflächenanlage in Herford-Diebrock geht es voran. \nNachdem der Bau- und Umweltausschuss bereits am 8. September einstimmig den nächsten Schritt im Genehmigungsverfahren ermöglicht und die Auslegung des Vorentwurfs zur Flächennutzungsplanänderung beschlossen hat, können wir uns nun auch über eine Förderzusage des Landes NRW freuen. In der vergangenen Woche traf der Förderbescheid über rund 372.000 Euro ein. Die Förderung erfolgt über das Programm 'progres.nrw', das den Bau von Freiflächen-Photovoltaikanlagen außerhalb des Erneuerbare-Energien-Gesetzes (EEG) - also ohne garantierte Einspeisevergütung - ermöglichen soll. Wir freuen uns sehr über die Förderzusage, da das Projekt mittlerweile Kosten von rund 2,4 Millionen Euro hat. Auch die bisher immer einstimmig gefallenen positiven Entscheidungen im Bau- und Umweltausschuss der Stadt Herford im Rahmen der bürokratischen Prozesse sind sehr ermutigend. Sie zeigen, dass die lokale Politik hinter unserem Engagement zum Ausbau der Erneuerbaren Energien steht."], 
-            img: ["content/pictures/Landesfoerderung.jpg"],
+            img: ["content/fegh/pictures/Landesfoerderung.jpg"],
             imgBody: [],
             href: ""
         },
@@ -43,7 +43,7 @@ var getNewsList = function() {
             newTo: "2023-09-20",
             title: "Coaching-Abschluss in Kassel", 
             body: ["Heute fand das Abschluss-Event unseres Coachings vom energewiende jetzt e.V. statt. Ingo und Fabian haben unsere Erfahrungen und Entwicklungen vorgestellt. Das Treffen heute sowie das ganze Coaching haben viel Spaß gemacht und uns wirklich voran gebracht. Besonders der Austausch mit den anderen Energiegenossenschaften war heute eine große Bereicherung."], 
-            img: ["content/pictures/Coaching-Gruppe.jpg"],
+            img: ["content/fegh/pictures/Coaching-Gruppe.jpg"],
             imgBody: [],
             href: ""
         },
@@ -53,8 +53,8 @@ var getNewsList = function() {
             title: "2. Radtour zum Reesberg und Hainkamp", 
             body: ["Heute fand unsere zweite Radtour vom Hainkamp in Herford Diebrock zum Reesberg in Kirchlengern statt. Barbara Rodi vom Vorstand erläuterte den aktuellen Stand zur geplanten großen 2,4 MegaWatt Photovoltaikanlage auf der ehemaligen Boden- und Bauschuttdeponie Am Hainkamp. Diese PV- Anlage wollen wir aus eigener Kraft finanzieren und werben fleißig um weitere Personen, die sich beteiligen möchten.",
                    "Es war ideales Radelwetter. Auf dem Reesberg angekommen war die Gruppe kleiner, aber umso feiner! \nVor Ort wurde dem Vorstand direkt eine Beitrittserklärung überreicht und die Interessenbekundung, sich mit einem qualifizierten Nachrangdarlehen an der neuen großen Photvoltaikanlage beteiligen zu wollen."], 
-            img: ["content/pictures/teilnehmende-der-radtour-stehen-im-kreis-um-barbara-rodi-vorstand-der-energiegenossenschaft.jpg",
-                  "content/pictures/6-personen-oben-auf-dem-reesberg-mit-solarmodulen-im-hintergrund-das-panorama-kirchlengern-und-löhne.jpg"],
+            img: ["content/fegh/pictures/teilnehmende-der-radtour-stehen-im-kreis-um-barbara-rodi-vorstand-der-energiegenossenschaft.jpg",
+                  "content/fegh/pictures/6-personen-oben-auf-dem-reesberg-mit-solarmodulen-im-hintergrund-das-panorama-kirchlengern-und-löhne.jpg"],
             imgBody: ["Interessierte lauschen den Worten von Barbara Rodi zur geplanten PV- Anlage",
                       "Am Ende der Führung auf dem Reesberg"],
             href: ""
@@ -65,7 +65,7 @@ var getNewsList = function() {
             newTo: "2022-12-25",
             title: "Interview mit Barbara Rodi bei Kanal-21", 
             body: ["Jörg Brökel, früherer Chefredakteur von Radio Herford, hat Barbara Rodi aus unserem Vorstand im Rahmen von 'gibt's', einer Sendung des Kanal-21 aus Bielefeld, zu den Themen Energiewende und Öko-Strom interviewt. Ab Minute 19 beginnt das Interview und dauert 10 Minuten.\nHier der Link zum Beitrag."], 
-            img: ["fotos/Kanal21-Interview.png"],
+            img: ["content/fegh/Kanal21-Interview.png"],
             imgBody: [],
             href: ""
         },
@@ -74,7 +74,7 @@ var getNewsList = function() {
             newTo: "2022-12-19",
             title: "PRESSEARTIKEL VOM BALKONKRAFTWERKE-WORKSHOP FÜR FRAUEN", 
             body: [], 
-            img: ["content/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
+            img: ["content/fegh/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
             imgBody: [],
             href: ""
         },
@@ -83,7 +83,7 @@ var getNewsList = function() {
             newTo: "2022-06-27",
             title: "Workshop für Frauen zu Balkonkraftwerken am 2.7.22", 
             body: ["Am Samstag, den 2. Juli 2022 um 15 Uhr findet im Mehrgenerationenhaus Ottelau ein Workshop für Frauen zu Balkonkraftwerken statt. Balkonkraftwerke sind Steckersolarmodule, die Strom aus Sonneneinstrahlung erzeugen. Bis zu 600 Watt Leistung können sie einfach in eine Steckdose gesteckt werden – wie ein Haushaltsgerät. Der Vorteil: damit kann eine Grundlast abgedeckt werden und die Stromrechnung sinkt. Außerdem trägt jedes Steckersolarmodul zum Klimaschutz bei. Es kann auch in den Garten gestellt werden oder auf die Terasse. Einige Formalien müssen beachtet und evtl. ein Zähler ausgetauscht werden.\n\nZiel des Workshops ist, dass jede Frau danach Bescheid weiß, wie das Gerät funktioniert, wie es installiert wird und wie die Anmeldung beim Netzbetreiber und Marktstammdatenregister gemacht wird."], 
-            img: ["content/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
+            img: ["content/fegh/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
             imgBody: ["...damit zukünftig mehr Frauen auf dem Bild zu sehen sind"],
             href: "app/html/new/frauenBalkonkraftwerk-Referentin.html"
         },
@@ -92,7 +92,7 @@ var getNewsList = function() {
             newTo: "2022-05-30",
             title: "Radtour zum Reesberg und Hainkamp am 29.5.22", 
             body: [], 
-            img: ["content/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
+            img: ["content/fegh/pictures/foto-mit-den-15-teilnehmenden-frauen-am-balkonkraftwerke-workshop-mit-balkonkraftwerk.jpg"],
             imgBody: ['Die "Neue Westfälische" vom 21. Mai 2022'],
             href: ""
         },
@@ -101,7 +101,7 @@ var getNewsList = function() {
             newTo: "2021-12-31",
             title: "UNSERE NÄCHSTE GROSSE PHOTOVOLTAIKANLAGE IST IN PLANUNG !", 
             body: ["Bei strahlendem Sonnenschein hatten sich Kinder mit ihren Eltern auf dem Gelände des zukünftigen Bürger*innsolarparks auf die Suche nach Ostereiern gemacht. Zwei Kinder fanden besonders markierte Eier, mit denen sie einen Anteil an unserer Bürgerenergiegenossenschaft geschenkt bekamen. Bei Kaffee und leckerem Kuchen der Biobäckerei Farina, der mit Bürger:innenstrom aus unseren PV-Anlagen gebacken wurde, wurde gefachsimpelt und über die FEGH erzählt. Auch Nachbar:innen der zukünftigen 2,4 MW großen PV-Anlage waren unserer Einladung gefolgt."], 
-            img: ["content/pictures/ostereiersuche-am-ostermontag-mit-einigen-teilnehmenden-und-aktiven-der-fegh-eg.jpg"],
+            img: ["content/fegh/pictures/ostereiersuche-am-ostermontag-mit-einigen-teilnehmenden-und-aktiven-der-fegh-eg.jpg"],
             imgBody: ['Ostereiersuche am Ostermontag mit einigen Teilnehmenden und Aktiven der FEGH eG'],
             href: ""
         }
@@ -121,47 +121,47 @@ var getNewsList = function() {
  */        
 var getNaviList = function() {
     var naviList = [
-        {id:"navStart", label: "Start", href: "#!/home", url: "app/html/home.html",
+        {id:"navStart", label: "Start", href: "#!/home", url: "content/fegh/html/home.html",
             subm: []},
 
-        {id:"navErg", label: "Öko-Energie", href: "#!/erg",  url: "content/html/erg.html",
-            subm: [ {label: "Bürger-Ökostrom", href: "#!/erg/1", url: "content/html/erg/erg0001.html"},
-                    {label: "Bürger-Ökogas", href: "#!/erg/2", url: "content/html/erg/erg0002.html"}                                                        
+        {id:"navErg", label: "Öko-Energie", href: "#!/erg",  url: "content/fegh/html/erg.html",
+            subm: [ {label: "Bürger-Ökostrom", href: "#!/erg/1", url: "content/fegh/html/erg/erg0001.html"},
+                    {label: "Bürger-Ökogas", href: "#!/erg/2", url: "content/fegh/html/erg/erg0002.html"}                                                        
                                                ]},
-        {id:"navWir", label: "Wir", href: "#!/wir", url: "content/html/wir.html",
-            subm: [ {label: "Kontakt", href: "#!/wir/1", url: "content/html/wir/wir0001.html"},
-                    {label: "Philosophie", href: "#!/wir/2", url: "content/html/wir/wir0002.html"}, 
-                    {label: "Satzung", href: "#!/wir/3", url: "content/html/wir/wir0003.html"}
+        {id:"navWir", label: "Wir", href: "#!/wir", url: "content/fegh/html/wir.html",
+            subm: [ {label: "Kontakt", href: "#!/wir/1", url: "content/fegh/html/wir/wir0001.html"},
+                    {label: "Philosophie", href: "#!/wir/2", url: "content/fegh/html/wir/wir0002.html"}, 
+                    {label: "Satzung", href: "#!/wir/3", url: "content/fegh/html/wir/wir0003.html"}
                                              ]},                                                    
-        {id:"navNews", label: "News", href: "#!/new", url: "content/html/news.html", 
-            subm: [ {label: "Archiv", href: "#!/new/1", url: "content/html/new/newArchiv.html"}]},
+        {id:"navNews", label: "News", href: "#!/new", url: "content/fegh/html/news.html", 
+            subm: [ {label: "Archiv", href: "#!/new/1", url: "content/fegh/html/new/newArchiv.html"}]},
 
-        {id:"navProj", label: "Projekte", href: "#!/pro", url: "content/html/projects.html", 
-            subm: [ {id: "navProHk", label: "Hainkamp", href: "#!/pro/hk", url: "content/html/pro/Hainkamp.html", 
-                        subm: [{label: "Tagebuch", href: "#!/pro/hk/tb", url: "content/html/pro/HainkampTagebuch.html"}]},
+        {id:"navProj", label: "Projekte", href: "#!/pro", url: "content/fegh/html/projects.html", 
+            subm: [ {id: "navProHk", label: "Hainkamp", href: "#!/pro/hk", url: "content/fegh/html/pro/Hainkamp.html", 
+                        subm: [{label: "Tagebuch", href: "#!/pro/hk/tb", url: "content/fegh/html/pro/HainkampTagebuch.html"}]},
                 
                     {id: "navProD", label: "Bauschuttdeponie Reesberg", href: "#!/pro/1", url: "content/html/pro/Bauschuttdeponie-Reesberg.html", 
-                        subm: [{label: "Projektbeschreibung", href: "#!/pro/1/1", url: "content/html/pro/ProjektbeschreibungR.html"}]},
-                    {label: "Erich-Gutenberg- Berufskolleg", href: "#!/pro/2", url: "content/html/pro/pro0002.html"},
-                    {label: "Bergs Hof", href: "#!/pro/3", url: "content/html/pro/pro0003.html"},
-                    {label: "Kindergarten Haus Lukas", href: "#!/pro/4", url: "content/html/pro/pro0004.html"},
-                    {label: "Kreissporthalle Herford", href: "#!/pro/5", url: "content/html/pro/pro0005.html"},
-                    {label: "Luise Scheppler Kindergarten", href: "#!/pro/6", url: "content/html/pro/pro0006.html"},
-                    {label: "B u. S Bünde", href: "#!/pro/7", url: "content/html/pro/pro0007.html"},
-                    {label: "Gärtnerei Ulenburg und Familie Esser", href: "#!/pro/8", url: "content/html/pro/pro0008.html"},
-                    {label: "Reiterhof Kahre", href: "#!/pro/9", url: "content/html/pro/pro0009.html"},
-                    {label: "Bahnhof Bieren", href: "#!/pro/10", url: "content/html/pro/pro0010.html",
-                        subm: [{label: "Details 1", href: "#!/pro/10/1", url: "content/html/pro/pro0010-1.html"},
-                               {label: "Details 2", href: "#!/pro/10/2", url: "content/html/pro/pro0010-2.html"}]},
-                    {label: "Familie Beversdorff", href: "#!/pro/XX", url: "content/html/pro/pro00XX.html"}
+                        subm: [{label: "Projektbeschreibung", href: "#!/pro/1/1", url: "content/fegh/html/pro/ProjektbeschreibungR.html"}]},
+                    {label: "Erich-Gutenberg- Berufskolleg", href: "#!/pro/2", url: "content/fegh/html/pro/pro0002.html"},
+                    {label: "Bergs Hof", href: "#!/pro/3", url: "content/fegh/html/pro/pro0003.html"},
+                    {label: "Kindergarten Haus Lukas", href: "#!/pro/4", url: "content/fegh/html/pro/pro0004.html"},
+                    {label: "Kreissporthalle Herford", href: "#!/pro/5", url: "content/fegh/html/pro/pro0005.html"},
+                    {label: "Luise Scheppler Kindergarten", href: "#!/pro/6", url: "content/fegh/html/pro/pro0006.html"},
+                    {label: "B u. S Bünde", href: "#!/pro/7", url: "content/fegh/html/pro/pro0007.html"},
+                    {label: "Gärtnerei Ulenburg und Familie Esser", href: "#!/pro/8", url: "content/fegh/html/pro/pro0008.html"},
+                    {label: "Reiterhof Kahre", href: "#!/pro/9", url: "content/fegh/html/pro/pro0009.html"},
+                    {label: "Bahnhof Bieren", href: "#!/pro/10", url: "content/fegh/html/pro/pro0010.html",
+                        subm: [{label: "Details 1", href: "#!/pro/10/1", url: "content/fegh/html/pro/pro0010-1.html"},
+                               {label: "Details 2", href: "#!/pro/10/2", url: "content/fegh/html/pro/pro0010-2.html"}]},
+                    {label: "Familie Beversdorff", href: "#!/pro/XX", url: "content/fegh/html/pro/pro00XX.html"}
             ]},
 
-        {id:"navSrv", label: "Service", href: "#!/srv", url: "content/html/service.html", 
-            subm: [ {label: "Beititt", href: "#!/srv/1", url: "content/html/srv/srv0001.html"},
-                    {label: "Anteilserhöhung", href: "#!/srv/2", url: "content/html/srv/srv0002.html"},
-                    {label: "Downloads", href: "#!/srv/3", url: "content/html/srv/srv0003.html"},
-                    {label: "Impressum", href: "#!/impressum", url: "content/html/Impressum.html"},
-                    {label: "Cookies", href: "#!/cookies", url: "content/html/CookiesNotRequired.html"}
+        {id:"navSrv", label: "Service", href: "#!/srv", url: "content/fegh/html/service.html", 
+            subm: [ {label: "Beititt", href: "#!/srv/1", url: "content/fegh/html/srv/srv0001.html"},
+                    {label: "Anteilserhöhung", href: "#!/srv/2", url: "content/fegh/html/srv/srv0002.html"},
+                    {label: "Downloads", href: "#!/srv/3", url: "content/fegh/html/srv/srv0003.html"},
+                    {label: "Impressum", href: "#!/impressum", url: "content/fegh/html/Impressum.html"},
+                    {label: "Cookies", href: "#!/cookies", url: "content/fegh/html/CookiesNotRequired.html"}
                   ]
         }
             
@@ -182,13 +182,13 @@ var getImgBoxList = function() {
             imgBodyList: ["Luftaufnahme mit geplanter Panelen-Aufteilung",
                            "Mitglieder mit Plakat",
                            "Hainkamp Karte der Gemarkung"],
-            imgList: ["content/pictures/pro/HainkampLuftaufnahme.png",
-                           "content/pictures/pro/GruppeMitLogoPlakat.jpg",
-                           "content/pictures/pro/HainkampKarte.png"]
+            imgList: ["content/fegh/pictures/pro/HainkampLuftaufnahme.png",
+                           "content/fegh/pictures/pro/GruppeMitLogoPlakat.jpg",
+                           "content/fegh/pictures/pro/HainkampKarte.png"]
         },
         {   imgKey: 'Home',
             imgBodyList: ["Mitglieder der Genossenschaft"],
-            imgList: ["content/pictures/gruppenfoto-fegheg.jpg"]
+            imgList: ["content/fegh/pictures/gruppenfoto-fegheg.jpg"]
         },
         {   imgKey: 'anyOther',
             imgBodyList: [],
@@ -209,7 +209,7 @@ var getProjectList = function() {
         {
             title: "ERICH-GUTENBERG- BERUFSKOLLEG",
             href: "#!/pro/1",
-            templateUrl: "content/html/pro/pro0001"
+            templateUrl: "content/fegh/html/pro/pro0001"
         }
 
     ];            
@@ -219,13 +219,13 @@ var getProjectList = function() {
 // Baground pictures
 var getObjBg = function() {        
     var key = ["home", "erg", "wir", "new", "pro", "srv", "impressum"];
-    var pic = [ "url(content/pictures/Solardach-mit-Blume.jpg)", 
-            "url(content/pictures/Solaranlage-mit-hängenden-Kablen.jpg)", 
-            "url(content/pictures/Solardach-im-Winter.jpg)", 
-            "url( content/pictures/Lichter-am-Haus.jpg)", 
-            "url( content/pictures/große-photovoltaik-anlage.jpg)", 
-            "url( content/pictures/Solarreinigung-auf-einem-Dach-Download.jpg)", 
-            "url( content/pictures/gruppenfoto-fegheg.jpg)"
+    var pic = [ "url(content/fegh/pictures/Solardach-mit-Blume.jpg)", 
+            "url(content/fegh/pictures/Solaranlage-mit-hängenden-Kablen.jpg)", 
+            "url(content/fegh/pictures/Solardach-im-Winter.jpg)", 
+            "url( content/fegh/pictures/Lichter-am-Haus.jpg)", 
+            "url( content/fegh/pictures/große-photovoltaik-anlage.jpg)", 
+            "url( content/fegh/pictures/Solarreinigung-auf-einem-Dach-Download.jpg)", 
+            "url( content/fegh/pictures/gruppenfoto-fegheg.jpg)"
         ];
     var objBg = {};
 
