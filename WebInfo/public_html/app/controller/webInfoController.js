7eb039cd9145e9d06487708f07f906125c169545
Egbert <Egbert@LenoAleks>
2023-03-11 17:56:48 +0100

    Bootstrap icon download is available

--- WebInfo/public_html/app/controller/webInfoController.js
+++ WebInfo/public_html/app/controller/webInfoController.js
@@ -34,11 +34,27 @@ var webInfoController =  function($rootScope, $scope, navSrv) {
     $scope.compSideMaps = function() {
         let sm = {"siteMap":"", "siteMapImg": ""};
         sm = $scope.navSrv.computeSiteMaps($rootScope);
-        console.log("siteMap.xml");
+        console.log("sitemap.xml");
         console.log(sm.siteMap);
-        console.log("siteMapImages.xml");
+        console.log("sitemapimages.xml");
         console.log(sm.siteMapImg);
     };
+    $scope.downloadSiteMaps = function() {
+        let sm = {"siteMap":"", "siteMapImg": ""};
+        sm = $scope.navSrv.computeSiteMaps($rootScope);
+        let xml = sm.siteMap;
+        let fileName = "sitemap.xml";
+        var dwn = function(fileName, xml) {
+            // https://stackoverflow.com/questions/25631738/using-javascript-to-store-a-file-to-a-local-machine-downloading
+            a = document.createElement('a');
+            a.setAttribute("href", "data:application/xml;charset=utf-8," + xml);
+            a.setAttribute("download", fileName);
+            a.click();
+            a.click();
+        };
+        dwn("sitemap.xml", sm.siteMap);
+        dwn("sitemapimages.xml", sm.siteMapImg);
+    };    
         
 //    // Test
 //    console.log("1 - webInfoController($scope)");
