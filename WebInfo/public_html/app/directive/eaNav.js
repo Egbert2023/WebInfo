6dba29864bdf0159a36efe6183d335b1144ca53e
Egbert <Egbert@LenoAleks>
2023-04-12 16:39:26 +0200

    eaNavDynDirektive and siteMap2 are ready, The documentation has been updated

--- WebInfo/public_html/app/directive/eaNav.js
+++ WebInfo/public_html/app/directive/eaNav.js
@@ -1,40 +1,4 @@
 'use strict';
-    
-var eaNavDirektive = function($rootScope, $http, $location, $compile, eaNavSrv) {
-    
-    //var urlIsReaded = false;
-    
-    return {
-        restrict: 'E',
-        templateUrl: "app/template/navbar.html",
-        controller: function($rootScope, $scope, $http) {
-            
-            $scope.scope_eaNavDirektive_Controller = $scope.url;
-            
-            $rootScope.$on("LoadJsonFile-naviList", function(evt, opt) {
-                $scope.scope_eaNavDirektive_Controller = $scope.url;    
-                $scope.naviList = $rootScope.naviList;
-                $scope.url = $scope.navSrv.getHtml4Id($rootScope, $location.path(), eaNavSrv);
-                //urlIsReaded = true;
-                
-                $rootScope.$emit("ReadUrlIsReady", $scope.url);
-            });
-            
-        },      
-        
-        link: function (scope, element, attr) {
-            let navLogo = attr.navLogo;
-            scope.$parent.navLogo = scope.$parent.contentFolder + navLogo;
-            scope.isActive = function(nav) {
-                return nav.href.indexOf(scope.location) === 1;
-            };
-            
-            $rootScope.$on("$locationChangeSuccess", function(event, next, current) {            
-                scope.location = $location.path();
-            });
-        }            
-     };
-};
 
 var eaNavDynDirektive = function($rootScope, $http, $location, $compile, eaNavSrv) {
     
