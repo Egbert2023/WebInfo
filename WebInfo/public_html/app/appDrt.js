d25014cca9e8d00b59b84f33789abca29d7cd99c
Egbert <Egbert@LenoAleks>
2024-01-14 17:49:37 +0100

    Insert application eaDirective

---
+++ WebInfo/public_html/app/appDrt.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//var eaDir = angular.module("eaDir", ["ngRoute", "ngCookies"]);
+var eaDir = angular.module("eaDir", ["ngRoute"]);
+
+var configSrv = eaDir.service('eaConfigSrv', [function($routeProvider){
+    this.eaNavConfig = eaNavConfig;      
+}]);
+configSrv.$inject = ['$scope'];
+
+var navSrv = eaDir.service('eaNavSrv', ['$q', function () {
+    var vm = this;
+    vm.getHtml4Id = getHtml4Id;
+    vm.getHtml = getHtml;
+    vm.getCurrentLink = getCurrentLink;
+    vm.computeSiteMaps = computeSiteMaps;
+}]);
+navSrv.$inject = ['$scope', '$rootScope'];
+
+eaDir.directive('eaLoadJson', ['$rootScope', '$http', eaLoadJson]); //.$inject = ['$scope'];
+eaDir.directive('eaNavi', ['$rootScope', '$http', '$location', '$compile', eaNavDynDirektive]); //.$inject = ['$scope'];
+eaDir.directive('eaAddHtml', ['$rootScope', '$compile', '$http', eaAddHtmlDirective]); //.$inject = ['$scope'];
+eaDir.directive('eaPathLink', ['$rootScope', '$location', 'eaNavSrv', eaPathLinkDirective]); //.$inject = ['$scope'];
+eaDir.directive('eaFooter', ['$rootScope', eaFooterDirective]); //.$inject = ['$scope'];
+eaDir.directive('eaImgBox', ['$compile', '$rootScope', eaImgBox]); //.$inject = ['$scope'];
+eaDir.directive('eaImg', ['$rootScope', eaImg]); //.$inject = ['$scope'];
+eaDir.directive('eaAccCoat', ['$rootScope', eaAccCoat]); //.$inject = ['$scope'];
+eaDir.directive('eaAccKey', ['$compile', '$rootScope', eaAccKey]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskCode', ['$compile', eaMaskCode]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskHtml', [eaMaskHtml]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskCss', [eaMaskCss]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskJs', [eaMaskJs]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskJson', [eaMaskJson]); //.$inject = ['$scope'];
+eaDir.directive('eaMaskNo', [eaMaskNo]); 
+eaDir.directive('eaVideo', ['$rootScope', eaVideo]); //.$inject = ['$scope'];
+eaDir.directive('eaRegTest', [eaRegTest]); 
+
+eaDir.directive('eaProvideObj', ['$rootScope', eaProvideObj]);
+eaDir.directive('eaDeTicket',  [eaDeTicket]);
+eaDir.directive('eaNews', ['$rootScope', eaNews]);
+
+//eaDir.directive('eaCookies', ['$rootScope', '$cookies', '$compile', eaCookiesDirektive]).$inject = ['$scope'];
+
+// $compile, $rootScope
+eaDir.config(['$routeProvider', eaNavConfig]);
+
+eaDir.controller('eaDirController', ['$rootScope', '$scope', 'eaNavSrv', eaDirController])
+        .$inject = ['$scope'];
+eaDir.controller('eaNaviController', ['$rootScope', '$scope', '$location', 'eaNavSrv', eaNaviController])
+        .$inject = ['$scope'];
--- WebInfo/public_html/app/directive/eaFooter.js
+++ WebInfo/public_html/app/directive/eaFooter.js
@@ -1,16 +1,14 @@
 'use strict';
 
-// <ea-footer data-footer-url="content/html/footer.html"></ea-footer>
-
 var eaFooterDirective = function ($rootScope) {
   return {
     restrict: 'E',
-    scope: {
-         footerUrl: '@'
-      },
+    scope: false,
     template: '<div ng-include = "footerUrl"></div>',
     link: function (scope, ele, attrs) {
-        scope.footerUrl = attrs.footerUrl;
+        let inputFooterUrl = attrs.footerUrl;
+        let contentFolder = scope.$root.contentFolder;
+        scope.footerUrl = contentFolder + "/" + inputFooterUrl;
     }       
   };
 };
